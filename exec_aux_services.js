f92c82ee4d554670afe144713a82ab8b5b215c77
juanmi <jmcabrera@copado.com>
2022-01-03 19:56:44 +0100

    Add connection Planner-Server

--- exec_aux_services.js
+++ exec_aux_services.js
@@ -27,5 +27,5 @@ const mongoExpressPort = '3333'
 const mySqlPort = '4444'
 
 exec('MongoDB', `docker run --rm -d -p ${mongoPort}:27017 --name ${mongoContainer} mongo:4.4-bionic`)
-exec('MongoExpress', `docker run --link ${mongoContainer}:mongo -d -p ${mongoExpressPort}:8081 --name ${mongoExpressContainer} -e ME_CONFIG_MONGODB_URL="mongodb://mongo:${mongoPort}" mongo-express`)
+exec('MongoExpress', `docker run --rm --link ${mongoContainer}:mongo -d -p ${mongoExpressPort}:8081 --name ${mongoExpressContainer} -e ME_CONFIG_MONGODB_URL="mongodb://mongo:${mongoPort}" mongo-express`)
 exec('MySQL', `docker run --rm -d -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=eoloplantsDB -p ${mySqlPort}:3306 --name ${mySqlContainer} mysql:8.0.22`);
--- toposervice/README.md
+++ toposervice/README.md
@@ -2,6 +2,6 @@
 > docker run --rm -p 2222:27017 -d --name p3mongodb mongo:4.4-bionic
 
 ## start-up MONGO-EXPRESS
-> docker run --link p3mongodb:mongo -p 3333:8081 -e ME_CONFIG_MONGODB_URL="mongodb://mongo:2222" mongo-express
+> docker run --rm --link p3mongodb:mongo -p 3333:8081 -e ME_CONFIG_MONGODB_URL="mongodb://mongo:2222" mongo-express
 
 localhost:3333
